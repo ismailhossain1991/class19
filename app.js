08e70347e5a6c779c22a906b78157fece587d150
ismailhossain1991 <ismail017456@gmail.com>
2022-02-25 08:06:31 +0600

    all file upload

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ res.data.map((dev, index) => {
     <td>${ dev.email }</td>
     <td><img style="object-fit:cover; width:50px; height:50px;" src="${ dev.photo }" alt=""></td>
     <td>
-       <a data-bs-toggle="modal" class="btn btn-info btn-sm" href="#modal_view"><i class="fa fa-eye"></i></a> 
+       <a data-bs-toggle="modal" class="btn btn-info btn-sm" onclick="modalSingleData(${ dev.id })" href="#modal_view"><i class="fa fa-eye"></i></a> 
        <a data-bs-toggle="modal" class="btn btn-warning btn-sm" onclick="editDeveloper(${ dev.id})" href="#modal_edit"><i class="fa fa-edit"></i></a> 
-       <a data-bs-toggle="modal" class="btn btn-danger btn-sm" href="#modal_delete"><i class="fa fa-trash"></i></a> 
+       <a data-bs-toggle="modal" class="btn btn-danger btn-sm" onclick="modalDeleteData(${ dev.id })" href="#modal_delete"><i class="fa fa-trash"></i></a> 
     </td>
    </tr>
     
@@ -139,3 +139,59 @@ getDevelopers();
 
 
  });
+
+ 
+ /**
+  * Delete Data
+  */
+  const modal_delete = document.getElementById('deldata');
+
+ function modalDeleteData(id){
+modal_delete.setAttribute('delId', id);
+
+console.log(modal_delete);
+ }
+
+modal_delete.addEventListener('click', function(){
+let del_id = this.getAttribute('delId');
+axios.delete(`http://localhost:5050/developers/${ del_id }`).then( res => {
+   
+    getDevelopers();
+});
+ });
+
+
+ /**
+  * Modal single Data
+  */
+
+ function modalSingleData(id){
+
+let modal_view = document.getElementById('modal_view');
+axios.get(`http://localhost:5050/developers/${ id }`).then( data => {
+
+axios.get(`http://localhost:5050/skill/${id}`).then(skill => {
+
+    modal_view.querySelector('.modal-body').innerHTML = `
+    <div class="card">
+        <img class="card-img" src="${data.data.photo}" alt="">
+        <div class="card-body">
+        <h2>${data.data.name}</h2>
+        <h3>${skill.data.name}</h3>
+        
+        </div>
+        </div>
+    
+    `;
+    
+}); 
+        
+
+   
+
+    });
+
+ }
+
+
+ 
